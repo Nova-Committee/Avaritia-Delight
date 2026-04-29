89efe465a78fdc5604a6fa58fd16bb632a95781c
cu6 <3077476144@qq.com>
2026-04-29 23:02:02 +0800

    1.0

---
+++ src/main/java/committee/nova/avaritia_delight/AvaritiaDelight.java
@@ -0,0 +1,17 @@
+package committee.nova.avaritia_delight;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(AvaritiaDelight.MOD_ID)
+public class AvaritiaDelight {
+
+    public static final String MOD_ID = "avaritia_delight";
+
+    public AvaritiaDelight(IEventBus modEventBus, ModContainer modContainer) {
+
+    }
+
+}
